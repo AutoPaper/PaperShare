c299b06ce1e912ad309b637295372d4c49ce0296
NNUZhangZhi <1557983850@qq.com>
2014-10-27 00:02:27 +0800

    比较蛋疼的前端设计
    忙了很长时间，也只能忙这么点。走的弯路太多了，不过估计以后会好点

--- AutoPaper/AutoPaper/App_Start/BundleConfig.cs
+++ AutoPaper/AutoPaper/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace AutoPaper
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css",
+                      "~/Content/buttons.css",
+                      "~/Content/mystyle.css"));
         }
     }
 }
