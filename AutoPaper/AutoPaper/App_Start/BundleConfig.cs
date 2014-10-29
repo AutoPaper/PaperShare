8e0f511eb862672fbe3119b11baa3299a2e8d45e
NNUZhangZhi <1557983850@qq.com>
2014-10-29 12:41:00 +0800

    又有了点进展

--- AutoPaper/AutoPaper/App_Start/BundleConfig.cs
+++ AutoPaper/AutoPaper/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace AutoPaper
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/buttons.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,9 +25,9 @@ namespace AutoPaper
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/buttons.css",
                       "~/Content/bootstrap.css",
                       "~/Content/Site.css",
-                      "~/Content/buttons.css",
                       "~/Content/mystyle.css"));
         }
     }
