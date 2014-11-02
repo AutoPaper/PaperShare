d61d7b3a35f56d70cf57a4c4444e8dc73da37166
NNUZhangZhi <1557983850@qq.com>
2014-11-02 23:33:28 +0800

    登录界面的js实现（部分）

--- AutoPaper/AutoPaper/App_Start/BundleConfig.cs
+++ AutoPaper/AutoPaper/App_Start/BundleConfig.cs
@@ -10,7 +10,8 @@ namespace AutoPaper
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/buttons.js"));
+                        "~/Scripts/buttons.js",
+                        "~/Scripts/jquery.lightbox.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
