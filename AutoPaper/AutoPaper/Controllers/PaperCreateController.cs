80e8e5d3b4cebcf3a1f377b61bcccc6b2a45cbaa
NNUZhangZhi <1557983850@qq.com>
2014-11-03 14:40:34 +0800

    完善了登录界面

---
+++ AutoPaper/AutoPaper/Controllers/PaperCreateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoPaper.Controllers
+{
+    public class PaperCreateController : Controller
+    {
+        //
+        // GET: /PaperCreate/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
