ab88a4d1e8c3eb04b89622538d99eb143ea9170e
iammodest <844713856@qq.com>
2014-12-07 09:42:27 +0800

    第二次提交

--- AutoPaper/AutoPaper/Controllers/IndividualCentreController.cs
+++ AutoPaper/AutoPaper/Controllers/IndividualCentreController.cs
@@ -16,6 +16,11 @@ namespace AutoPaper.Controllers
 
         public ActionResult Default()
         {
+            int userID = Convert.ToInt32(Request.Cookies["userID"].Value);
+            var noticesList = ((from o in db.notice_table
+                                where o.userID == userID
+                                select o)).ToList<notices>();
+            ViewBag.noticesList = noticesList;
             return View();
         }
         public ActionResult Account()
@@ -24,7 +29,24 @@ namespace AutoPaper.Controllers
         }
         public bool KeyChangeConfirm()
         {
-            return true;
+            int userID = Convert.ToInt32(Request.Cookies["userID"].Value);
+            string oldPassword = Request.Form["user-key"];
+            string newPassword1 = Request.Form["user-new-key"];
+            string newPassword2 = Request.Form["user-new-key-confirm"];
+            //判断用户输入密码是否匹配
+            var Users = ((from o in db.user_table
+                          where o.ID == userID
+                          select o)).ToArray<users>();
+
+            //匹配成功，更新数据库，返回true
+            if (Users[0].keyhash == oldPassword)
+                if (newPassword1 == newPassword2)
+                {
+                    Users[0].keyhash = newPassword1;
+                    db.SaveChanges();
+                    return true;
+                }
+            return false;
         }
         public ActionResult MyPapers()
         {
@@ -32,14 +54,38 @@ namespace AutoPaper.Controllers
         }
         public ActionResult MyCollectQuestion()
         {
+            //获取用户id
+            int userID = Convert.ToInt32(Request.Cookies["userID"].Value);
+            //找出收藏试卷id
+            var questionList = (from a in db.question_table
+                                join b in db.QU_table
+                                on a.ID equals b.questionID
+                                where b.userID == userID
+                                select a).ToList<question>();
+            ViewBag.questionList = questionList;
             return View();
         }
         public ActionResult MyCollectPapers()
         {
+            //获取用户id
+            int userID = Convert.ToInt32(Request.Cookies["userID"].Value);
+            //找出收藏试卷id
+            var paperList = (from a in db.paper_table
+                             join b in db.PU_table
+                             on a.ID equals b.paperID
+                             where b.userID == userID && b.logAddr == null
+                             select a).ToList<papers>();
+            ViewBag.paperList = paperList;
             return View();
         }
         public ActionResult MyErrorsFound()
         {
+            //获取用户id
+            int userID = Convert.ToInt32(Request.Cookies["userID"].Value);
+            var MyErrorsFoundList = (from o in db.error_table
+                                     where o.fromID == userID
+                                     select o).ToList<errorHistory>();
+            ViewBag.MyErrorsFoundList = MyErrorsFoundList;
             return View();
         }
     }
